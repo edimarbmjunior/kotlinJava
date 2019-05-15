dcca8a12ae22c766cfb32f65a81fb8d4c38db6a0
Edimar Junior <edimarbmjunior@gmail.com>
2019-05-15 11:42:57 -0300

    Populando a tela de detalhes.

--- src/main/java/ui/postDetailForm.java
+++ src/main/java/ui/postDetailForm.java
@@ -1,20 +1,35 @@
 package ui;
 
+import business.PostBusiness;
+import entity.PostEntity;
+
 import javax.swing.*;
 
 public class postDetailForm extends JFrame{
     private JPanel rootPanel;
     private JLabel Titulo;
-    private JLabel lblTitulo;
-    private JLabel lblBody;
+    private JLabel lblTitulo = new JLabel();
+    private JLabel lblBody = new JLabel();
+
+    private PostBusiness mPostBusiness = new PostBusiness();
 
     public postDetailForm(int id) {
         super("Minha Janela");
 
         rootPanel = new JPanel();
+        this.loadPost(id);
+        /*this.rootPanel.add(this.lblBody);
+        this.rootPanel.add(this.lblTitulo);*/
 
         this.setSize(500, 250);
         this.setVisible(true);
         this.setContentPane(rootPanel);
+    }
+
+    private  void loadPost(int id){
+        PostEntity entity = this.mPostBusiness.getSinglePost(id);
+
+        this.lblTitulo.setText(entity.getTitle());
+        this.lblBody.setText("<html>" + entity.getBody() + "</html>");
     }
 }
